4118e778ae37955fb917e478b435a0d83ed4b5a6
Boris Kodel <boris.kodel@gmail.com>
2013-03-13 16:34:02 +0200

    update basic module again

--- src/examples/BasicModule.java
+++ src/examples/BasicModule.java
@@ -4,9 +4,9 @@ import types.Module;
 
 public class BasicModule extends Module {
 	public BasicModule() {
-		this.add("cmd input", CmdInput.class);
-		this.add("cmd output", CmdOutput.class);
-		this.add("is lower", IsLower.class);
-		this.add("is upper", IsUpper.class);
+		this.add(new CmdInput());
+		this.add(new CmdOutput());
+		this.add(new IsLower());
+		this.add(new IsUpper());
 	}
 }
