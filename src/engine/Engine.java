9916eadcead371446ed5e73c4a98322aeda0231f
shai dykan <shai.dorit@gmail.com>
2013-03-13 09:12:48 +0200

    new jaxb classes

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,9 +1,23 @@
 package engine;
 
+import genrated.Flow;
+import genrated.Flow.Command;
+import genrated.ObjectFactory;
+
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
 
 public class Engine {
+	Flow flow;
 	/*Input input;
 	Output output;
 	ArrayList<Executer> flow;
@@ -22,4 +36,26 @@ public class Engine {
 		
 		output.write(data);
 	}*/
+	
+	public Engine(String xml){
+		StringReader xmlReader = new StringReader(xml);
+		StreamSource xmlSource = new StreamSource(xmlReader);
+		try{
+			JAXBContext content = JAXBContext.newInstance(Flow.class);
+			Unmarshaller unmarshaller = content.createUnmarshaller();
+			flow = (Flow)((JAXBElement)unmarshaller.unmarshal(xmlSource,Flow.class)).getValue();
+		} catch (JAXBException e){
+			System.out.println("STOP");
+		}
+		
+		List<Command> commands = 
+				GraphOrder.orginizeCommands(flow.getCommand());
+		
+		Command c=null;
+		c.getId();
+		c.getNext().getId();
+		
+		
+		
+	}
 }
--- src/engine/GraphOrder.java
+++ src/engine/GraphOrder.java
@@ -1,10 +1,14 @@
 package engine;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import genrated.Flow;
+import genrated.Flow.Command;
 
 public class GraphOrder {
 
-	public static ArrayList orginizeCommands(ArrayList commands){
-		return commands;
+	public static ArrayList<Integer> orginizeCommands(List<Command> commands){
+		return null;
 	}
 }
