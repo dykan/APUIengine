c03fc6a2f21d2f073d559930537619876cf796c0
shai dykan <shai.dorit@gmail.com>
2013-03-13 11:22:29 +0200

    prediacate

--- src/engine/NodePredicate.java
+++ src/engine/NodePredicate.java
@@ -11,6 +11,22 @@ public class NodePredicate extends NodeCommand{
 	public NodePredicate(BasicCommand command) {
 		super(command);
 	}
+
+	public NodeCommand getTrueCommand() {
+		return trueCommand;
+	}
+
+	public void setTrueCommand(NodeCommand trueCommand) {
+		this.trueCommand = trueCommand;
+	}
+
+	public NodeCommand getFalseCommand() {
+		return falseCommand;
+	}
+
+	public void setFalseCommand(NodeCommand falseCommand) {
+		this.falseCommand = falseCommand;
+	}
 	
 
 }
