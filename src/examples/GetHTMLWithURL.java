3a6ab33c58a67ef3c45828ee9ae143f3ba9703d6
AnatLichtenstein <anatlichtenstein@gmail.com>
2013-03-14 08:51:11 +0200

    Adding more executers

---
+++ src/examples/GetHTMLWithURL.java
@@ -0,0 +1,56 @@
+package examples;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import types.Executer;
+import types.FlowData;
+
+public class GetHTMLWithURL implements Executer{
+
+	@Override
+	public String getLabel() {
+		return "Get source code of an HTML code";
+	}
+
+	@Override
+	public Class[] getInputTypes() {
+		return new Class[]{String.class};
+	}
+
+	@Override
+	public FlowData execute(FlowData data) {
+		String strURL = data.toString();
+		return new FlowData((Object)getUrlSource(strURL));
+	}
+
+	@Override
+	public Class getOutputType() {
+		return String.class;
+	}
+	
+	private static String getUrlSource(String strUrl) {
+        
+		StringBuilder strBuilder = null;
+		try {
+			URL url = new URL(strUrl);
+	        URLConnection yc = url.openConnection();
+	        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
+	        String inputLine;
+	        strBuilder = new StringBuilder();
+	        while ((inputLine = in.readLine()) != null)
+	            strBuilder.append(inputLine);
+	        in.close();
+        }
+        catch (IOException e){
+        	e.printStackTrace();
+        }
+        finally {
+        	
+        }
+        return strBuilder.toString();
+    }
+}
